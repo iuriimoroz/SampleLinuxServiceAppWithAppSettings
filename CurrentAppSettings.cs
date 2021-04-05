5e07e855d13b75d635b59c3dd600e82e5457726a
moroz <moroz@axinom.com>
2021-04-05 17:23:55 +0300

    Add project files.

---
+++ CurrentAppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleService
+{
+    public class CurrentAppSettings
+    {
+        public bool IsEnabled { get; set; }
+        public string Timeout { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using SampleService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Service.Sample
+{
+    class Program
+    {
+        private static readonly string outFolderPath = "/var/log/SampleService";
+        private static readonly string outFilePath = Path.Combine(outFolderPath, $"{DateTime.Now:yyyy-MM-dd}.txt");
+        static void Main()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+            var section = config.GetSection(nameof(CurrentAppSettings));
+            var currentAppSettings = section.Get<CurrentAppSettings>();    
+
+            if (!Directory.Exists(outFolderPath))
+            {
+                Directory.CreateDirectory(outFolderPath);
+            }
+            Console.WriteLine("Following settings applied during app startup:");
+            Console.WriteLine($"Set app in constant loop: {currentAppSettings.IsEnabled}");
+            Console.WriteLine($"App timeout: {currentAppSettings.Timeout}");
+            Console.WriteLine($"Output file: {outFilePath}");
+            Console.WriteLine();
+
+            File.AppendAllLines(outFilePath, new List<string>() { "------------------- SERVICE START ------------------- " }, Encoding.UTF8);
+
+            while (currentAppSettings.IsEnabled)
+            {
+                File.AppendAllLines(outFilePath, new List<string>() { DateTime.Now.ToString() }, Encoding.UTF8);
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(Convert.ToInt32(currentAppSettings.Timeout));
+            }
+        }
+    }
+}
